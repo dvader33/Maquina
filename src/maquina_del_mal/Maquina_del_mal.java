5b94b879287f1d1c019fdb7f2070918c23a70c0b
dvader33 <vhader33.dr@gmail.com>
2015-07-07 11:24:53 -0600

    Ejecutando aplicacion

--- src/maquina_del_mal/Maquina_del_mal.java
+++ src/maquina_del_mal/Maquina_del_mal.java
@@ -5,6 +5,9 @@
  */
 package maquina_del_mal;
 
+import Estructura.*;
+import java.util.ArrayList;
+
 /**
  *
  * @author Vader33
@@ -15,6 +18,30 @@ public class Maquina_del_mal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        EstadoInicial ini= new EstadoInicial();
+        GiroInicial giro= new GiroInicial();
+        DevolverDulce dulce = new DevolverDulce();
+            
+        ArrayList<Estado> lista= new ArrayList<>();
+        lista.add(ini);
+        lista.add(giro);
+        lista.add(dulce);
+        
+        
+        
+        MaquinaDulce maquina= new MaquinaDulce(lista);
+        
+        maquina.procesar();
+        maquina.procesar();
+        maquina.procesar();
+        
+        maquina.setMoneda(true);
+        
+        maquina.procesar();
+        maquina.procesar();
+        maquina.procesar();
+        
+        
         // TODO code application logic here
     }
     
